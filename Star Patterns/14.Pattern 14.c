29f7023c7101aa09b1dacd47f9427bf9596e9ba7
M.Arun prasath <70267488+techyarun@users.noreply.github.com>
2021-08-27 19:07:01 +0530

    Create 14.Pattern 14.c

---
+++ Star Patterns/14.Pattern 14.c
@@ -0,0 +1,36 @@
+//printing star pattern --> (model 14)
+//Hollow square pattern
+<--------------------
+//	//OUTPUT Value
+//	//if input=5
+ * * * * *
+ *       *
+ *       *
+ *       *
+ * * * * *
+-------------------->	
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	//variable declaration
+	int i,j,input;
+	
+	//user input
+	printf("Enter the number of rows: ");
+	scanf("%d",&input);
+	
+	//logic
+	for(i=1;i<=input;i++){
+		for(j=1;j<=input;j++){
+			if(i==1 || i==input || j==1 || j==input){
+				printf("* ");
+			}else{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
