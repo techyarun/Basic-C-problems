50ea86a2321066ddf9757df6596d2f3acfa197f0
M.Arun prasath <70267488+techyarun@users.noreply.github.com>
2021-08-27 19:30:23 +0530

    Create 15.Pattern 15.c

---
+++ Star Patterns/15.Pattern 15.c
@@ -0,0 +1,37 @@
+//printing star pattern --> (model 12)
+//triangle pattern
+<--------------------
+	//OUTPUT Value
+	//if input=5
+	       *
+	     * * *
+	   * * * * *
+	 * * * * * * *
+ * * * * * * * * *
+-------------------->	
+  
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	//variable declaration
+	int i,j,input,space,temp;
+	
+	//user input
+	printf("Enter the number of rows: ");
+	scanf("%d",&input);
+	
+	//logic
+	temp=input;
+	for(i=1;i<=input*2;i=i+2){
+		for(space=temp;space>=0;space--){
+			printf("  ");
+		}
+		for(j=1;j<=i;j++){
+			printf("* ");
+		}
+		printf("\n");
+		temp--;
+	}
+	return 0;
+}
