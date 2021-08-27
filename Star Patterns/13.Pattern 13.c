fdab8a26a7719de9c74deda6e150574e51e927f2
M.Arun prasath <70267488+techyarun@users.noreply.github.com>
2021-08-27 18:58:24 +0530

    Create 13.Pattern 13.c

---
+++ Star Patterns/13.Pattern 13.c
@@ -0,0 +1,32 @@
+//printing star pattern --> (model 12)
+//square pattern
+<--------------------
+	//OUTPUT Value
+	//if input=5
+  1 1 1 1 1
+  2 2 2 2 2
+  3 3 3 3 3
+  4 4 4 4 4
+  5 5 5 5 5
+-------------------->	
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	//variable declaration
+	int i,j,input;
+	
+	//user input
+	printf("Enter the number of rows: ");
+	scanf("%d",&input);
+	
+	//logic
+	for(i=1;i<=input;i++){
+		for(j=1;j<=input;j++){
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
