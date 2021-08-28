3c3b41a17801db2fdb9653b558c69714cecd43a6
M.Arun prasath <70267488+techyarun@users.noreply.github.com>
2021-08-28 10:13:43 +0530

    Create Biggest number using array.c

---
+++ Problems/Biggest number using array.c
@@ -0,0 +1,31 @@
+//Biggest number using an arrays
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	//variable declarations
+	int input,i,a[10],big;
+	
+	//user input
+	printf("Enter the input: ");
+	scanf("%d",&input);
+	
+	//logic
+	//to get the value
+	for(i=0;i<input;i++){
+		scanf("%d",&a[i]);
+	}
+	big=a[0];
+	//to check wither it big or Not
+	for(i=0;i<input;i++){
+		if(a[i]>big){
+			big=a[i];
+		}
+	}
+	
+	//print the output
+	printf("Big number is :%d",big);
+	
+	return 0;
+}
