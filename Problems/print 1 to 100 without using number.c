24321e0a6ffcea81454156a9abe49dab47962781
M.Arun prasath <70267488+techyarun@users.noreply.github.com>
2021-08-28 12:23:01 +0530

    Create print 1 to 100 without using number.c

---
+++ Problems/print 1 to 100 without using number.c
@@ -0,0 +1,15 @@
+//print 1 to 100 without using number
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	char n='A'/'A';
+	int i;
+	
+	//logic
+	for(i=n;i<='d';i++){
+		printf("%d\n",i);
+	}
+	return 0;
+}
